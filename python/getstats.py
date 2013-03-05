1d5b24ce43de7cc90d56959ef732128d64966f16
Nicholas Shelly <nshelly@cs.stanford.edu>
2013-03-04 16:56:40 -0800

    Polynomial fit.

--- python/getstats.py
+++ python/getstats.py
@@ -2,21 +2,46 @@ import os
 import sys
 import time
 from socket import gethostname
+import os, platform, subprocess, re
 
 sys.path.append("../swig")
 
 import snap as Snap
 
 MIN_NODES_EXPONENT = 1
-MAX_NODES_EXPONENT = 4
-NUM_ITERATIONS = 4
-PLOT_TYPES = range(6)  # Max of 8
-GRAPH_TYPES = (0, 3, 4)
+MAX_NODES_EXPONENT = 5
+NUM_ITERATIONS = 10
+PLOT_TYPES = [1]   # Max of 8
+GRAPH_TYPES = [4]    # Just RMAT for now
 HOSTNAME = gethostname().split('.')[0]
+AVG_DEG = 3
 #GRAPH_TYPES = 5
 
 RESULTS_DIR = "results-%s" % HOSTNAME
 #RESULTS_DIR = "results-%s" % time.strftime("%m-%d-%H%M.%S")
+
+#def get_processor_name():
+#  
+#  os_string = sys.platform
+#  
+#  if 'win' in os_string:
+#    return platform.processor()
+#  elif 'darwin' in os_string:
+#    import os
+#    os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
+#    command = "sysctl -n machdep.cpu.brand_string"
+#    brand = subprocess.check_output(command).strip()
+#    command = "sysctl -n hw.cpufrequency"
+#    speed = float(subprocess.check_output(command).strip()) / 10**9
+#    return "%s %.2f" % (brand, speed)
+#  elif 'linux' in os_string:
+#    command = "cat /proc/cpuinfo"
+#    all_info = subprocess.check_output(command, shell=True).strip()
+#    for line in allInfo.split("\n"):
+#      if "model name" in line:
+#        return re.sub( ".*model name.*:", "", line,1)
+#  else:
+#    return "unknown"
 
 def calc_stats():
   
@@ -30,8 +55,9 @@ def calc_stats():
         NNodes = randrange(10**e,10**(e+1))
         
         # Random number of edges (from 1-3x nodes)
-        NEdges = randrange(NNodes, NNodes*3)
-        
+#        NEdges = randrange(NNodes, NNodes*3)
+        NEdges = NNodes*AVG_DEG
+      
         print "NNodes=%.2e, %.2e" % (NNodes, NEdges)
         
         # Repeat for all graph types
@@ -50,16 +76,101 @@ def calc_stats():
 # run tests on infolab machines
 # run simple calibration program, then provide estimate
 
+import matplotlib
+matplotlib.use('Agg')
+
+from pylab import plot,loglog,show,xlabel,ylabel,savefig,title,figure,legend,grid
+from numpy import sort,array,ones,linalg,column_stack,loadtxt
+from scipy import *
+from scipy import optimize
+
+  
+##########
+# Fitting the data -- Least Squares Method
+##########
+def poly_fit(Xdata, Ydata, label):
+  
+  X = Xdata
+  Y = Ydata
+
+  # define our (line) fitting function
+  fitfunc = lambda p, x: p[0] + p[1] * x
+  #  errfunc = lambda p, x, y, err: (y - fitfunc(p, x)) / err
+  errfunc = lambda p, x, y: (y - fitfunc(p, x))
+
+  pinit = [1.0, 1.0]
+  pfinal,covar,infodict,mesg,ier = \
+      optimize.leastsq(errfunc, pinit, args=(X, Y), full_output=1)
+  
+  print "pfinal = ", pfinal
+  print "covar: \n", covar
+  
+  ss_err=(infodict['fvec']**2).sum()
+  ss_tot=((Y-X.mean())**2).sum()
+  rsquared=1-(ss_err/ss_tot)
+  print "Poly: rsquared = ", rsquared
+  
+  index = pfinal[1]
+  amp = 10.0**pfinal[0]
+  
+  indexErr = sqrt( covar[0][0] )
+  ampErr = sqrt( covar[1][1] ) * amp
+  plot(X, Y, 'o', label=label)
+  
+  grid(True)
+  Y_fit = fitfunc(pfinal,X)
+  plot(X, Y_fit, '-', label=("%s-poly" % label))
+
+def powerlaw_fit(Xdata, Ydata, label):
+# Power-law fitting is best done by first converting
+# to a linear equation and then fitting to a straight line.
+#
+#  y = a * x^b
+#  log(y) = log(a) + b*log(x)
+#
+
+#  X = log10(Xdata)
+  X = Xdata
+#  Y = log10(Ydata)
+  Y = Ydata
+#  logyerr = yerr / ydata
+
+  # define our (line) fitting function
+  fitfunc = lambda p, x: p[0] + p[1] * x
+#  errfunc = lambda p, x, y, err: (y - fitfunc(p, x)) / err
+  errfunc = lambda p, x, y: (y - fitfunc(p, x))
+
+  pinit = [1.0, -1.0]
+  pfinal,covar,infodict,mesg,ier = optimize.leastsq( \
+                    errfunc, pinit, args=(X, Y), full_output=1)
+
+  print "pfinal = ", pfinal
+  print "covar = ", covar
+  
+  ss_err=(infodict['fvec']**2).sum()
+  ss_tot=((Y-X.mean())**2).sum()
+  rsquared=1-(ss_err/ss_tot)
+  print "rsquared = ", rsquared
+
+  index = pfinal[1]
+  amp = 10.0**pfinal[0]
+
+  indexErr = sqrt( covar[0][0] )
+  ampErr = sqrt( covar[1][1] ) * amp
+  loglog(X, Y, 'o', label=label)
+  
+  grid(True)
+  Y_fit = fitfunc(pfinal,X)
+  plot(Xdata, Y_fit, '-', label=("%s-power" % label))
+
+#def func(x, a, b, c):
+#  return a * numpy.exp(-b * x) + c
+
 def plot_stats():
   
-  from numpy import sort,array,ones,linalg,column_stack,loadtxt
-  import matplotlib
-  matplotlib.use('Agg')
-  from pylab import plot,show,xlabel,ylabel,savefig,title,figure,legend
-
   for type in PLOT_TYPES:
-    figure()
     
+    figure()
     for g in GRAPH_TYPES:
     
       fname = '%s/%s-%s.txt' % (RESULTS_DIR, Snap.GetGraphAbbr(g),
@@ -74,19 +185,31 @@ def plot_stats():
       X = column_stack([X, ones(len(X))])
 
       # Fit using least-squares
-      w = linalg.lstsq(X,Y)[0] # obtaining the parameters
-      print "N = %d" % len(X)
-      print "coefficients = ", w
+#      logX = log10(X)
+#      logY = log10(Y)
+#      w = linalg.lstsq(logX,logY)[0] # obtaining the parameters
+#      print "N = %d" % len(X)
+#      print "coefficients = ", w
+#      # Plot the least-squares line
+#      Y_fit = w[0]*logX[:,0] + w[1]*logX[:,1] + w[2] # get regression line
       
-      # Plot the line
-      line = w[0]*X[:,0] + w[1]*X[:,1] + w[2] # get regression line
-      plot(X[:,0], Y,'o', label=Snap.GetGraphDesc(g))
-      plot(X[:,0], line, label="%s-fit" % Snap.GetGraphDesc(g))
-      legend()
-      xlabel('Num Nodes')
-      ylabel('time (2.6 GHz)')
+      # Fit using Levenburg-Marquardt algorithm through leastsq
+#      p0 = scipy.array([1,1,1])
+#      coeffs, matcov = curve_fit(func, X[:,0], Y, p0)
+#      Y_fit = func(x, coeffs[0], coeffs[1], coeffs[2])
+
+#      powerlaw_fit(X[:,0], Y, "%s-fit" % Snap.GetGraphDesc(g))
+      poly_fit(X[:,0], Y, "%s" % Snap.GetGraphDesc(g))
+
+      
+#      loglog(X[:,0], Y,'o', label=Snap.GetGraphDesc(g))
+#      loglog(X[:,0], Y_fit, label="%s-fit" % Snap.GetGraphDesc(g))
+      legend(loc='lower right')
+      xlabel('Num Nodes (d_avg = %.1f)' % AVG_DEG)
+      ylabel('time')
+      
       title('%s run time (%s)' % (Snap.GetAttributeDesc(type), HOSTNAME))
-    
+
     #end for loop - graph type
   
     pname = '%s/plot_%s.png' % (RESULTS_DIR, Snap.GetAttributeAbbr(type))
@@ -94,8 +217,8 @@ def plot_stats():
   
     savefig(pname)
 
-    coeff_file = open('%s/coeff.txt' % RESULTS_DIR, 'w+')
-    coeff_file.write('%d %.4f %.4f %.4f\n' % (type, w[0], w[1], w[2]))
+#    coeff_file = open('%s/coeff.txt' % RESULTS_DIR, 'w+')
+#    coeff_file.write('%d %.4f %.4f %.4f\n' % (type, w[0], w[1], w[2]))
 
   #end for loop - plot type
 
@@ -103,12 +226,17 @@ if __name__ == '__main__':
   
   if not os.path.exists(RESULTS_DIR):
     os.makedirs(RESULTS_DIR)
+  
+#  for n in range(NUM_ITERATIONS):
+#
+#    calc_stats()
+#    
+#    # Update plots every 5 iterations
+#    if (n+1) % 2 == 0:
+#      print "Iteration %d of %d:" % (n+1, NUM_ITERATIONS)
+#      plot_stats()
 
-  for n in range(NUM_ITERATIONS):
+  plot_stats()
 
-    calc_stats()
-    # Update plots every 5 iterations
-    if (n+1) % 2 == 0:
-      print "Iteration %d of %d:" % (n+1, NUM_ITERATIONS)
-      plot_stats()
+
 
--- swig/getassessment.cpp
+++ swig/getassessment.cpp
@@ -27,7 +27,8 @@ const char* const GraphDesc[] = {
 };
 
 typedef enum {
-  Info,     /* graph info */
+  Iteration,     /* graph info */
+  Triads,   /* get triads */
   PlotDD,   /* degree distribution */
   PlotCDD,  /* cumulative degree */
   PlotHop,  /* hop plot (diameter) */
@@ -40,7 +41,8 @@ typedef enum {
 } PlotType;
 
 const char* const PlotAbbr[] = {
-  "info", /* basic graph info (e.g. iteration, triads) */
+  "iterations", /* basic graph info (e.g. iteration, triads) */
+  "triads", /* triads */
   "DD",   /* degree distribution */
   "CDD",  /* cumulative degree */
   "HOP",  /* hop plot (diameter) */
@@ -52,7 +54,8 @@ const char* const PlotAbbr[] = {
 };
 
 const char* const PlotDesc[] = {
-  "basic graph info (w. node/edge iteration, triads)",
+  "node/edge iteration",
+  "triads",
   "cumulative degree",
   "hop plot (diameter)",
   "distribution of weakly connected components",
@@ -62,10 +65,45 @@ const char* const PlotDesc[] = {
   "left and right singular vector"  ,
 };
 
-using namespace Snap;
+using namespace TSnap;
 
 typedef TVec<TInt, int> TIntV;
 typedef TVec<TIntV, int> TIntIntVV;
+
+template <class PGraph>
+void RunBasicInfo(const PGraph& Graph, const TStr& Desc, const TStr& OutFNm) {
+  int BiDirEdges=0, ZeroNodes=0, ZeroInNodes=0, ZeroOutNodes=0, SelfEdges=0, NonZIODegNodes=0;
+  THash<TIntPr, TInt> UniqDirE, UniqUnDirE;
+  for (typename PGraph::TObj::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    if (NI.GetDeg()==0) ZeroNodes++;
+    if (NI.GetInDeg()==0) ZeroInNodes++;
+    if (NI.GetOutDeg()==0) ZeroOutNodes++;
+    if (NI.GetInDeg()!=0 && NI.GetOutDeg()!=0) NonZIODegNodes++;
+    const int NId = NI.GetId();
+    for (int edge = 0; edge < NI.GetOutDeg(); edge++) {
+      const int DstNId = NI.GetOutNId(edge);
+      if (Graph->IsEdge(DstNId, NId)) BiDirEdges++;
+      if (NId == DstNId) SelfEdges++;
+      UniqDirE.AddKey(TIntPr(NId, DstNId));
+      UniqUnDirE.AddKey(TIntPr(TInt::GetMn(NId, DstNId), TInt::GetMx(NId, DstNId)));
+    }
+  }
+}
+
+template <class PGraph>
+void RunTriads(const PGraph& Graph) {
+  int64 Closed=0, Open=0;
+  GetTriads(Graph, Closed, Open);
+}
+
+template <class PGraph>
+void RunClustCf(const PGraph& Graph) {
+  TFltPrV DegToCCfV;
+  int64 ClosedTriads, OpenTriads;
+  const double CCF = GetClustCf(Graph, DegToCCfV, ClosedTriads, OpenTriads);
+  #pragma unused(CCF)
+  
+}
 
 template<class PGraph>
 void RunCalculations(const PGraph& Graph, PlotType PType) {
@@ -75,10 +113,15 @@ void RunCalculations(const PGraph& Graph, PlotType PType) {
                            Graph->GetNodes()/2;
   printf("Calculating '%s'\n", PlotDesc[PType]);
   switch (PType) {
-    case Info:
-      PrintInfo(Graph, Desc, OutFNm, 0); // Not fast option
+    case Iteration:
+//      PrintInfo(Graph, Desc, OutFNm, 0); // Not fast option
+      RunBasicInfo(Graph, Desc, OutFNm);
       break;
       
+    case Triads:
+      RunTriads(Graph);
+      break;
+
     case PlotDD:
       PlotOutDegDistr(Graph, OutFNm, Desc, false, false);
       PlotInDegDistr(Graph, OutFNm, Desc, false, false);
@@ -102,7 +145,8 @@ void RunCalculations(const PGraph& Graph, PlotType PType) {
       break;
       
     case PlotClustCoef:
-      PlotClustCf(Graph, OutFNm, Desc);
+//      PlotClustCf(Graph, OutFNm, Desc);
+      RunClustCf(Graph);
       break;
       
     case PlotSVal:
@@ -122,7 +166,7 @@ void RunCalculations(const PGraph& Graph, PlotType PType) {
 double GetStats(int NNodes, int NEdges, PlotType PType, GraphType RType) {
   
   TExeTm ExeTm;
-  printf("Timing '%s': Time: %s\n", PlotDesc[PType], TExeTm::GetCurTm());
+  printf("Timing '%s'\n", PlotDesc[PType]);
   
   int StartTime = clock();
   
