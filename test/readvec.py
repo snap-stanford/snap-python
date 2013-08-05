4d4c47d111728df25a3196b115e96ba672a3ac29
Rok Sosic <rok@cs.stanford.edu>
2013-08-05 10:59:08 -0700

    vector code updates

--- test/readvec.py
+++ test/readvec.py
@@ -5,12 +5,20 @@ import time
 sys.path.append("/home/rok/git/rok/snapworld")
 import snap as Snap
 
-fname = "/home/rok/git/rok/snapworld/work-nbr/swout-GetNbr-0-GetDist-0.txt"
+if __name__ == '__main__':
 
-FIn = Snap.TFIn(Snap.TStr(fname))
-Vec = Snap.TIntV(FIn)
-print "len", Vec.Len()
+    if len(sys.argv) < 2:
+        print "Usage: " + sys.argv[0] + " <file>"
+        sys.exit(1)
 
-for i in range(0,Vec.Len()):
-    print "Vec", i, Vec.GetVal(i).Val
+    fname = sys.argv[1]
+
+    FIn = Snap.TFIn(Snap.TStr(fname))
+    Vec = Snap.TIntV(FIn)
+    print "len", Vec.Len()
+
+    Vec.Sort()
+
+    for i in range(0,Vec.Len()):
+        print "Vec", i, Vec.GetVal(i).Val
 
