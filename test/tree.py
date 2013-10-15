515658f0eb8b36a5897f6e1ad6caaedd95c78be8
Rok Sosic <rok@cs.stanford.edu>
2013-10-14 18:18:38 -0700

    added a short test program for IsTree()

---
+++ test/tree.py
@@ -0,0 +1,15 @@
+import snap
+
+tree = snap.GenTree(snap.PNGraph, 3, 5)
+print "nodes %d, edges %d" % (tree.GetNodes(), tree.GetEdges())
+
+result = snap.IsTree_PNGraph(tree)
+print "IsTree %s" % (str(result))
+
+istree = result[0]
+root = result[1]
+print "Result1 %d %d" % (istree, root)
+
+istree, root = snap.IsTree(tree)
+print "Result2 %d %d" % (istree, root)
+
