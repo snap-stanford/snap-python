bc0bbbbb4c128dfc245840b9b500c999b5bdd1ab
Sheila Ramaswamy <ramasshe@cs.stanford.edu>
2013-10-20 22:53:48 -0700

    Added tests for functions 4-11

---
+++ test/snap-test.py
@@ -0,0 +1,118 @@
+import unittest
+
+import snap
+
+class SnapPythonTest(unittest.TestCase):
+
+    def setUp(self):
+        self.num_nodes = 10
+
+        # Full Graphs
+        self.DirGraphFull = snap.GenFull(snap.PNGraph, self.num_nodes)
+        self.UnDirGraphFull = snap.GenFull(snap.PUNGraph, self.num_nodes)
+        self.NetFull = snap.GenFull(snap.PNEANet, self.num_nodes)
+
+        # Star Graphs
+        self.DirGraphStar = snap.GenStar(snap.PNGraph, self.num_nodes)
+        self.UnDirGraphStar = snap.GenStar(snap.PUNGraph, self.num_nodes)
+        self.NetStar = snap.GenStar(snap.PNEANet, self.num_nodes)
+
+    def test_CntInDegNodes(self):
+        # Directed graph
+        num_nodes = snap.CntInDegNodes(self.DirGraphFull, self.num_nodes-1)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+        # Undirected Graph
+        num_nodes = snap.CntInDegNodes(self.UnDirGraphFull, self.num_nodes-1)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+        # Network
+        num_nodes = snap.CntInDegNodes(self.NetFull, self.num_nodes-1)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+    def test_CntOutDegNodes(self):
+        # Directed Graph
+        num_nodes = snap.CntOutDegNodes(self.DirGraphFull, self.num_nodes-1)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+        # Undirected Graph
+        num_nodes = snap.CntOutDegNodes(self.UnDirGraphFull, self.num_nodes-1)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+        # Network
+        num_nodes = snap.CntOutDegNodes(self.NetFull, self.num_nodes-1)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+    def test_CntDegNodes(self):
+        # Directed Graph - it will have twice the edges as the undirected graph
+        num_nodes = snap.CntDegNodes(self.DirGraphFull, 2*(self.num_nodes-1))
+        self.assertEqual(self.num_nodes, num_nodes)
+
+        # Undirected Graph
+        num_nodes = snap.CntDegNodes(self.UnDirGraphFull, self.num_nodes-1)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+        # Network
+        num_nodes = snap.CntDegNodes(self.NetFull, 2*(self.num_nodes-1))
+        self.assertEqual(self.num_nodes, num_nodes)
+
+    def test_CntNonZNodes(self):
+        # Directed Graph
+        num_nodes = snap.CntNonZNodes(self.DirGraphFull)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+        # Undirected Graph
+        num_nodes = snap.CntNonZNodes(self.UnDirGraphFull)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+        # Network
+        num_nodes = snap.CntNonZNodes(self.NetFull)
+        self.assertEqual(self.num_nodes, num_nodes)
+
+    def test_CntEdgesToSet(self):
+        ######## Function not yet implemeted ########
+        pass
+
+    def test_GetMxDegNId(self):
+        # Directed Graph
+        max_id = snap.GetMxDegNId(self.DirGraphStar)
+        self.assertEqual(0, max_id)
+
+        # Undirected Graph
+        max_id = snap.GetMxDegNId(self.UnDirGraphStar)
+        self.assertEqual(0, max_id)
+
+        # Network
+        max_id = snap.GetMxDegNId(self.NetStar)
+        self.assertEqual(0, max_id)
+
+    def test_GetMxInDegNId(self):
+        # Directed Graph
+        max_id = snap.GetMxInDegNId(self.DirGraphStar)
+        # node with id 0 is the only node with in-degree 0
+        self.assertNotEqual(0, max_id)
+
+        # Undirected Graph
+        max_id = snap.GetMxInDegNId(self.UnDirGraphStar)
+        self.assertEqual(0, max_id)
+
+        # Network
+        max_id = snap.GetMxInDegNId(self.NetStar)
+        # node with id 0 is the only node with in-degree 0
+        self.assertNotEqual(0, max_id)
+
+    def test_GetMxOutDegNId(self):
+        # Directed Graph
+        max_id = snap.GetMxOutDegNId(self.DirGraphStar)
+        self.assertEqual(0, max_id)
+
+        # Undirected Graph
+        max_id = snap.GetMxOutDegNId(self.UnDirGraphStar)
+        self.assertEqual(0, max_id)
+
+        # Network
+        max_id = snap.GetMxOutDegNId(self.NetStar)
+        self.assertEqual(0, max_id)
+
+if __name__ == '__main__':
+  unittest.main()
