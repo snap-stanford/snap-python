45b4b4fe9180767df7a2f6082dfa68e7e07a6efd
Rok Sosic <rok@cs.stanford.edu>
2015-05-12 18:14:17 -0700

    - updated test files

--- test/test-snappy.sh
+++ test/test-snappy.sh
@@ -7,8 +7,22 @@
 #		p2p-Gnutella08.txt
 #
 
-scripts=( quick_test.py snap-test.py test-tnodei.py test-io.py \
-    intro.py tutorial.py tneanet.py cncom.py bfs.py attributes.py ) 
+# TODO
+#   bug-328-cnm.py
+#   bug-384-deledge.py
+#   bug-509-load.py
+#   bug-585-genrndpowerlaw.py
+scripts=(quick_test.py \
+    cncom.py intro.py tutorial.py tneanet.py bfs.py attributes.py \
+    test-tnodei.py test-io.py \
+    test-131-setiter.py \
+    test-346-getei.py \
+    test-356-getei.py \
+    test-374-addstrattrdate.py \
+    test-582-getnodewcc.py \
+    test-585-genrndpowerlaw.py \
+    bug-613-getbfstree.py \
+    snap-test.py) 
 
 for line in "${scripts[@]}"; do
     echo "***" `date` "$line ..."
@@ -19,3 +33,4 @@ for line in "${scripts[@]}"; do
         exit $RETVAL
     fi;
 done
+
