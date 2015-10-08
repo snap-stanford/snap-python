c993305f7475cbab9eb1fcd318d363e0984ba9f3
Rok Sosic <rok@cs.stanford.edu>
2015-10-07 19:40:51 -0700

    - added test on test-THashKeyDatI.py

---
+++ test/test-THashKeyDatI.py
@@ -0,0 +1,11 @@
+import snap
+#h1 = snap.TIntH()
+h1 = snap.TIntStrH()
+for i in range(500):
+    h1[i] = "a"
+
+it = h1.BegI()
+while not it.IsEnd():
+    print it.GetKey(), it.GetDat()
+    it.Next()
+
