37670b586773b68cd888277862eca1fd55537ee3
Rok Sosic <rok@cs.stanford.edu>
2016-09-14 12:37:59 -0700

    - added a test batch file for Snap.py on Windows

---
+++ test/test-snappy.bat
@@ -0,0 +1,31 @@
+rem test script for basic Snap.py functionality on Windows
+rem
+rem input file requirements, files are in "data"
+rem		p2p-Gnutella08.txt
+
+python quick_test.py
+python cncom.py
+python intro.py
+python tutorial.py
+python tneanet.py
+python bfs.py
+python attributes.py
+python test-tnodei.py
+python test-io.py
+python test-131-setiter.py
+python test-346-getei.py
+python test-356-getei.py
+python test-374-addstrattrdate.py
+python test-384-deledge.py
+python test-509-load.py
+python test-582-getnodewcc.py
+python test-585-genrndpowerlaw.py
+python test-613-getbfstree.py
+python test-vec-ops.py
+python test-2015-18a-attr.py
+python test-20160801-LoadConnListStr.py
+python test-GetBetweennessCentr.py
+python test-GetClosenessCentr.py
+python test-GetFarnessCentr.py
+python snap-test.py
+
