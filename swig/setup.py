5c534a3f5931a282d1abff6f8e7034524c850f89
Rok Sosic <rok@cs.stanford.edu>
2019-08-08 19:19:00 -0700

    - improved the package build process - renamed build targets in Makefile to better reflect the output format - changed the version to 5.2.0 - improved package naming for source and wheel packages in setup.py - minor cosmetic updates in setup.py

--- swig/setup.py
+++ swig/setup.py
@@ -12,15 +12,17 @@ import platform
 import sys
 
 #from distutils.core import setup, Extension
-from setuptools import setup, Extension
+#from setuptools import setup, Extension
 
-#added pip flag
-pip=False
+import setuptools
+
+# added pip flag
+swpip = False
 
 #
 #   Snap.py version
 #
-snappy_version = "4.1.0"
+snappy_version = "5.2.0"
 def getdynpath():
     '''
     get the path for the Python dynamic library
@@ -42,24 +44,33 @@ def getdynpath():
     return newpath
     
 # is this a dry run
+narg = 1
 dryrun = False
-if len(sys.argv) > 1  and  sys.argv[1] == "-n":
+if len(sys.argv) > narg  and  sys.argv[narg] == "-n":
     dryrun = True
+    narg += 1
 
-# is this a distribution build
-sdist = False
-if len(sys.argv) > 1  and  sys.argv[1] == "sdist":
-    sdist = True
+# is this a distribution package build
+swsdist = False
+if len(sys.argv) > narg  and  sys.argv[narg] == "sdist":
+    swsdist = True
+
+# is this a wheel build
+swwheel = False
+if len(sys.argv) > narg  and  sys.argv[narg] == "bdist_wheel":
+    swwheel = True
     
 # added OS specific wheeling
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-    class bdist_wheel(_bdist_wheel):
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-except ImportError:
-    bdist_wheel = None
+bdist_wheel = None
+if swwheel:
+    try:
+        from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+        class bdist_wheel(_bdist_wheel):
+            def finalize_options(self):
+                _bdist_wheel.finalize_options(self)
+                self.root_is_pure = False
+    except ImportError:
+        pass
 
 #
 #   determine package parameters:
@@ -109,7 +120,7 @@ if uname[0] == "Linux":
             swubuntu = True
     except:
         pass
-    useros="POSIX :: Linux"
+    user_os="POSIX :: Linux"
 
     obj_name = "_snap.so"
 
@@ -122,19 +133,19 @@ elif uname[0] == "Darwin":
         os_version = "macosx" + content.strip()
     except:
         pass
-    useros="MacOS"
+    user_os="MacOS"
     obj_name = "_snap.so"
 
 elif uname[0].find("CYGWIN") == 0:
     w = uname[0].rsplit("-",1)
     os_version = w[0].lower()
-    obj_name = "_snap.so"
-    useros="Microsoft :: Windows"
+    obj_name   = "_snap.so"
+    user_os    = "Microsoft :: Windows"
 
 elif uname[0].find("Windows") == 0:
     os_version = "Win"
-    obj_name = "_snap.pyd"
-    useros="Microsoft :: Windows"
+    obj_name   = "_snap.pyd"
+    user_os    = "Microsoft :: Windows"
 
 # architecture
 arch = "i386"
@@ -142,9 +153,12 @@ arch = "i386"
 if uname[4] == "x86_64"  or  uname[4] == "i686"  or  uname[4] == "AMD64":
     arch = "x64"
 
+# the tarball package name
 pkg_version = "-".join([snappy_version, snap_version,
-                        os_version, arch, python_version])
-
+		os_version, arch, python_version])
+if swwheel:
+   # pip requires that package names on different platforms are the same
+   pkg_version = snappy_version
 
 #
 #   get the installation directory
@@ -159,14 +173,14 @@ instdir = "site-packages"
 if swubuntu:
     instdir = "dist-packages"
 
-#pip path relative to usr/local/ for mac and linux
-if uname[0]=="Darwin":
-    pip_install='lib/python'+'/'+instdir+'/'
-elif uname[0]=="Linux":
-    pip_install='lib/python'+str(sys.version_info[0])+'.'+str(sys.version_info[1])+'/'+instdir+'/'
+# pip path relative to usr/local/ for mac and linux
+if uname[0] == "Darwin":
+    pip_install = os.path.join('lib/python', instdir)
+elif uname[0] == "Linux":
+    sysinfo = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
+    pip_install = os.path.join('lib/python', sysinfo, instdir)
 else:
-    pip_install='lib/python'+'/'+instdir+'/'
-
+    pip_install = os.path.join('lib/python', instdir)
 
 # check for an alternative Python user directory
 user_install = sys_install
@@ -182,22 +196,30 @@ if uname[0] == "Darwin":
     dynlib_path = getdynpath()
 
 if dryrun:
-    print("swubuntu", swubuntu)
-    print("pkg_version", pkg_version)
-    print("obj_name", obj_name)
-    print("user_install", user_install)
-    print("pip_install", pip_install)
+    print("snappy_version %s" % snappy_version)
+    print("snap_version %s" % snap_version)
+    print("os_version %s" % os_version)
+    print("arch %s" % arch)
+    print("python_version %s" % python_version)
+    print("user_os %s" % user_os)
+    print("swubuntu %s" % swubuntu)
+    print("pkg_version %s" % pkg_version)
+    print("obj_name %s" % obj_name)
+    print("user_install %s" % user_install)
+    print("sdist_flag %s" % swsdist)
+    print("wheel_flag %s" % swwheel)
+    print("pip_install %s" % pip_install)
     if dynlib_path:
-        print("dynlib_path", dynlib_path)
+        print("dynlib_path %s" % dynlib_path)
     sys.exit(0)
     
-#switch to pip directory
-if pip:
-    user_install=pip_install
+# switch to pip directory
+if swpip:
+    user_install = pip_install
 
 # specify additional files for Mac OS X
 files = []
-if uname[0] == "Darwin"  and  sdist:
+if uname[0] == "Darwin"  and  swsdist:
     files = ["install_name_tool", "update_dynlib.sh"]
 
 #
@@ -213,21 +235,21 @@ with open("README.txt", "r") as fh:
 #
 #   setup configuration
 #
-setup (name = 'snap',
+setuptools.setup(name = 'snap',
     py_modules  = ["snap"],
     #ext_modules = [snap_module],
     data_files  = [(user_install, [obj_name])], #user_install
     scripts     = files,
-    version     = snappy_version,
+    version     = pkg_version,
     author      = "snap.stanford.edu",
-    description = """SNAP (Stanford Network Analysis Platform) Python""",
-    long_description=long_description,
-    long_description_content_type="text/plain",
-    #url="https://github.com/snap-stanford/snap-python",
-    cmdclass={'bdist_wheel': bdist_wheel},
-    classifiers=[
-        "Programming Language :: Python :: "+str(sys.version_info[0]),
-        "Operating System :: " + useros,
+    description = '""SNAP (Stanford Network Analysis Platform) Python""',
+    long_description = long_description,
+    #long_description_content_type = "text/plain",
+    #url = "https://github.com/snap-stanford/snap-python",
+    cmdclass = {'bdist_wheel': bdist_wheel},
+    classifiers = [
+        "Programming Language :: Python :: " + str(sys.version_info[0]),
+        "Operating System :: " + user_os,
     ],
     )
 
