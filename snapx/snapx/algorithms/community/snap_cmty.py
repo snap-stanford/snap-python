e18b197106b43c8732c7e1d0430eb2f3572cdb36
dkumazaw <dkumazaw@stanford.edu>
2020-07-04 23:16:36 -0700

    Remove irrelevant comment

--- snapx/snapx/algorithms/community/snap_cmty.py
+++ snapx/snapx/algorithms/community/snap_cmty.py
@@ -3,12 +3,8 @@ from snap import TCnComV
 from snap import CommunityCNM, CommunityGirvanNewman
 
 def community_CNM(graph):
-    # TODO: How should we handle primitive 
-    # data structures like TCnComV?
-
     cmtyV = TCnComV()
     modularity = CommunityCNM(graph.snap_graph, cmtyV)
-    
     return (modularity, cmtyV)
 
 def community_girvan_newman(graph):
