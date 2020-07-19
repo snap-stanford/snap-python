817bc7306cc29feeae1bbf0c61976b8366b78cdf
Viswesh Krishna <krishnaviswesh@gmail.com>
2020-07-19 17:21:27 +0530

    add networkx exceptions

--- snapx/snapx/exception.py
+++ snapx/snapx/exception.py
@@ -7,14 +7,27 @@ __all__ = [
     "SnapXKeyError",
 ]
 
+
 class SnapXException(Exception):
     """Base class for exceptions in SnapX."""
+
 
 class SnapXError(SnapXException):
     """Exception for a serious error in SnapX"""
 
+
+class SnapXAlgorithmError(SnapXException):
+    """Exception for unexpected termination of algorithms."""
+
+
+class SnapXUnfeasible(SnapXAlgorithmError):
+    """Exception raised by algorithms trying to solve a problem
+    instance that has no feasible solution."""
+
+
 class SnapXTypeError(TypeError):
     """Exception for SNAP specific type errors"""
+
 
 class SnapXKeyError(KeyError):
     """Exception for SNAP specific key errors"""
