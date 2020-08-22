2c793b442439da82cc655ef6cf0b1e440a7b6a78
Viswesh Krishna <krishnaviswesh@gmail.com>
2020-08-22 19:56:39 +0530

    feat: get pneanet egonet with copy attributes and sampling

--- snapx/snapx/generators/ego.py
+++ snapx/snapx/generators/ego.py
@@ -7,7 +7,7 @@ import snapx as sx
 import snap
 
 
-def ego_graph(G, n, radius=1, center=True, undirected=False, distance=None):
+def ego_graph(G, n, radius=1, sample=-1.0, traversal='in', copy_attr=True):
     """PORTED FROM NETWORKX
     Returns induced subgraph of neighbors centered at node n within
     a given radius.
@@ -23,39 +23,47 @@ def ego_graph(G, n, radius=1, center=True, undirected=False, distance=None):
     radius : number, optional
       Include all neighbors of distance<=radius from n.
 
-    center : bool, optional
+    center : bool, optional - NOT ADDED
       If False, do not include center node in graph
 
-    undirected : bool, optional
+    undirected : bool, optional - NOT ADDED
       If True use both in- and out-neighbors of directed graphs.
 
-    distance : key, optional
+    distance : key, optional - NOT ADDED
       Use specified edge data key as distance.  For example, setting
       distance='weight' will use the edge weight to measure the
       distance from the node n.
+    
+    sample : int/float, optional
+      Number of nodes to sample as neighbors. Either positive int or float
+      between 0.0 and 1.0.
 
-    Notes
-    -----
-    For directed graphs D this produces the "out" neighborhood
-    or successors.  If you want the neighborhood of predecessors
-    first reverse the graph with D.reverse().  If you want both
-    directions use the keyword argument undirected=True.
+    traversal : str, optional, either 'in', 'out', 'all'
+      Get in, out or both ego neighborhoods
 
-    Node, edge, and graph attributes are copied to the returned subgraph.
+    copy_attr : bool, optional
+      Copy node and edge attributes to ego graph
     """
-    if undirected:
-        if distance is not None:
-            raise NotImplementedError
+    if traversal == 'in':
+        if copy_attr:
+            if sample != -1.0:
+                if type(sample) is int:
+                    if 0 > sample:
+                        raise RuntimeError
+                    else:
+                        snapGraph = snap.GetInEgonetSubAttr(G._graph, n, radius, sample, -1.0)
+                        return sx.Graph(incoming_graph_data=snapGraph)
+                else:
+                    if not 0.0 <= sample <= 1.0:
+                        raise RuntimeError
+                    else:
+                        snapGraph = snap.GetInEgonetSubAttr(G._graph, n, radius, 0, sample)
+                        return sx.Graph(incoming_graph_data=snapGraph)
+            else:
+                snapGraph = snap.GetInEgonetAttr(G._graph, n, radius)
+                return sx.Graph(incoming_graph_data=snapGraph)
         else:
-            # ret = snap.GetEgonet(H, n)
-            H = snap.ConvertGraph(snap.PNGraph, G._graph)
-            Nodes = snap.TIntV()
-            for N in H.GetNI(n).GetOutEdges():
-                Nodes.Add(N)
-            I = snap.GetSubGraphRenumber(H, Nodes)
-            J = sx.Graph()
-            J._graph = snap.ConvertGraph(snap.PNEANet, I)
-            return J
+            raise NotImplementedError
     else:
         raise NotImplementedError
 
